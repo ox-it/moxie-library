957007e609152334316b0f855795ca73d26312b0
Martin Filliau <martin@filliau.com>
2012-09-28 11:45:11 +0100

    Skeleton of library service.

---
+++ views.py
@@ -0,0 +1,9 @@
+from moxie.core.views import ServiceView
+
+class Search(ServiceView):
+    def handle_request(self):
+        pass
+
+class ResourceDetail(ServiceView):
+    def handle_request(self):
+        pass
