5ebf39de6b336a3510b9078d80c736c38dc22702
David King <dave@davbo.org>
2012-10-12 18:21:14 +0100

    Renamed blueprints due to namespace collision.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,11 @@ from flask import Blueprint
 
 from .views import Search, ResourceDetail
 
-library = Blueprint('library', __name__)
+library_blueprint = Blueprint('library', __name__)
 
 # URL Rules
-library.add_url_rule('/search', view_func=Search.as_view('search'))
-library.add_url_rule('/<path:id>', view_func=ResourceDetail.as_view('resourcedetail'))
+library_blueprint.add_url_rule('/search',
+        view_func=Search.as_view('search'))
+
+library_blueprint.add_url_rule('/<path:id>',
+        view_func=ResourceDetail.as_view('resourcedetail'))
