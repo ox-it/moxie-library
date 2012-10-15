1ae5e45e316a0ece09c76d2dc338bc45346eb241
David King <dave@davbo.org>
2012-10-15 16:27:22 +0100

    Created blueprint factories

--- __init__.py
+++ __init__.py
@@ -2,11 +2,14 @@ from flask import Blueprint
 
 from .views import Search, ResourceDetail
 
-library_blueprint = Blueprint('library', __name__)
 
-# URL Rules
-library_blueprint.add_url_rule('/search',
-        view_func=Search.as_view('search'))
+def create_blueprint(blueprint_name):
+    library_blueprint = Blueprint(blueprint_name, __name__)
 
-library_blueprint.add_url_rule('/<path:id>',
-        view_func=ResourceDetail.as_view('resourcedetail'))
+    # URL Rules
+    library_blueprint.add_url_rule('/search',
+            view_func=Search.as_view('search'))
+
+    library_blueprint.add_url_rule('/<path:id>',
+            view_func=ResourceDetail.as_view('resourcedetail'))
+    return library_blueprint
