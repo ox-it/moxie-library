00cfa0ac34cb4b03794d4164be2107e9e5ee10e5
Martin Filliau <martin@filliau.com>
2012-11-01 11:50:44 +0000

    Library / updated service to get provider.

--- services.py
+++ services.py
@@ -10,6 +10,15 @@ class LibrarySearchService(Service):
     """Library search service
     """
 
+    def __init__(self, providers=None):
+        self.providers = providers or []
+
+    def get_provider(self):
+        """Get a provider for searching libraries
+        TODO this currently returns the first provider in the list
+        """
+        return self.providers[0]
+
     def search(self, title, author, isbn):
         """Search for media in the given provider.
         :param title: title
@@ -17,7 +26,7 @@ class LibrarySearchService(Service):
         :param isbn: isbn
         :return list of results
         """
-        z = self.get_provider(None)
+        z = self.get_provider()
         q = LibrarySearchQuery(title, author, isbn)
         poi_service = POIService.from_context()
         results = z.library_search(q)
@@ -26,7 +35,6 @@ class LibrarySearchService(Service):
                 poi = poi_service.get_place_by_identifier('olis-aleph:{0}'.format(location.replace('/', '\/')))
                 if poi:
                     result['holdings'][location]['poi'] = simplify_doc_for_render(poi)
-                    result['holdings'][location]['poi']['@self'] = url_for('places.poidetail', ident=poi['id'])
         return results
 
     def get_media(self, control_number):
@@ -34,6 +42,6 @@ class LibrarySearchService(Service):
         :param control_number: ID of the media
         :return result or None
         """
-        z = self.get_provider(None)
+        z = self.get_provider()
         result = z.control_number_search(control_number)
         return result
