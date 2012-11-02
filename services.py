e3b71776cc65d32bd7b87e578dc635e30741cb2e
Martin Filliau <martin@filliau.com>
2012-11-02 10:46:28 +0000

    Library / moved some logic to services.

--- services.py
+++ services.py
@@ -1,14 +1,24 @@
+import hashlib
+import logging
+import json
+
 from flask import url_for
 
 from moxie.core.service import Service
+from moxie.core.kv import kv_store
 from moxie.library.providers.oxford_z3950 import LibrarySearchQuery
 from moxie.places.services import POIService
 from moxie.places.importers.helpers import simplify_doc_for_render
+
+logger = logging.getLogger(__name__)
 
 
 class LibrarySearchService(Service):
     """Library search service
     """
+
+    CACHE_KEY_FORMAT = '{0}_library_search_{1}'
+    CACHE_EXPIRE = 120   # seconds for cache to expire
 
     def __init__(self, providers=None):
         self.providers = providers or []
@@ -19,25 +29,41 @@ class LibrarySearchService(Service):
         """
         return self.providers[0]
 
-    def search(self, title, author, isbn):
+    def search(self, title, author, isbn, start=0, count=10, no_cache=False):
         """Search for media in the given provider.
         :param title: title
         :param author: author
         :param isbn: isbn
+        :param start: first result to return
+        :param count: number of results to return
+        :param no_cache: do not use the (potentially) cached result
         :return list of results
         """
-        z = self.get_provider()
-        q = LibrarySearchQuery(title, author, isbn)
+        search_string = "{0}{1}{2}".format(removeNonAscii(title), removeNonAscii(author), removeNonAscii(isbn))
+        hash = hashlib.md5()
+        hash.update(search_string)
+        cache = kv_store.get(self.CACHE_KEY_FORMAT.format(__name__, hash.hexdigest()))
+        if cache and not no_cache:
+            results = json.loads(cache)
+        else:
+            provider = self.get_provider()
+            query = LibrarySearchQuery(title, author, isbn)
+            results = provider.library_search(query)
+            kv_store.setex(self.CACHE_KEY_FORMAT.format(__name__, hash.hexdigest()), self.CACHE_EXPIRE, json.dumps(results))
+
         poi_service = POIService.from_context()
-        results = z.library_search(q)
-        for result in results:
-            result['_self'] = url_for('library.resourcedetail', id=result['control_number'])
+
+        page = list()
+
+        for result in results[start:(start+count)]:
+            result['links'] = { 'self': url_for('library.resourcedetail', id=result['control_number']) }
             for location in result['holdings']:
                 # TODO place identifier has to be set in configuration
                 poi = poi_service.get_place_by_identifier('olis-aleph:{0}'.format(location.replace('/', '\/')))
                 if poi:
                     result['holdings'][location]['poi'] = simplify_doc_for_render(poi)
-        return results
+            page.append(result)
+        return len(results), page
 
     def get_media(self, control_number):
         """Get a media by its control number
@@ -47,3 +73,10 @@ class LibrarySearchService(Service):
         z = self.get_provider()
         result = z.control_number_search(control_number)
         return result
+
+
+def removeNonAscii(s):
+    if s:
+        return "".join(i for i in s if ord(i)<128)
+    else:
+        return ""
--- views.py
+++ views.py
@@ -21,53 +21,27 @@ class Search(ServiceView):
         title = request.args.get('title', None)
         author = request.args.get('author', None)
         isbn = request.args.get('isbn', None)
+        start = int(request.args.get('start', 0))
+        count = int(request.args.get('count', 10))
 
         # TODO shouldn't be as general as Exception, could be Bad Request if it's inconsistent query
         # or Service Unavailable if the service is... not available...
         try:
-            results = self.get_search_result(title, author, isbn)
+            service = LibrarySearchService.from_context()
+            size, results = service.search(title, author, isbn, start, count)
         except Exception as e:
             abort(400, description=e.message)
         else:
             # 2. Do pagination
-            start = int(request.args.get('start', 0))
-            count = int(request.args.get('count', 10))
-
-            context = { 'size': len(results),
-                        'results': results[start:(start+count)] }
-            # TODO add links: first, prev, last in addition to next
-            if len(results) > start+count:
-                context['links'] = { 'next': url_for('.search', title=title, author=author, isbn=isbn, start=start+count, count=count)}
+            context = { 'size': size,
+                        'results': results,
+                        'links': dict()}
+            # TODO add links: prev, last in addition to next
+            if size > start+count:
+                context['links']['next'] = url_for('.search', title=title, author=author, isbn=isbn, start=start+count, count=count)
+            context['links']['last'] = url_for('.search', title=title, author=author, isbn=isbn, start=size-count, count=count)
+            context['links']['first'] = url_for('.search', title=title, author=author, isbn=isbn, count=count)
             return context
-
-    def get_search_result(self, title, author, isbn):
-        """Check the cache or call the service to retrieve
-        search results
-        :param title: search title
-        :param author: search author
-        :param isbn: search isbn
-        :return list of results
-        """
-        search_string = "{0}{1}{2}".format(removeNonAscii(title), removeNonAscii(author), removeNonAscii(isbn))
-        hash = hashlib.md5()
-        hash.update(search_string)
-        service = LibrarySearchService.from_context()
-        cache = kv_store.get(self.CACHE_KEY_FORMAT.format(__name__, hash.hexdigest()))
-        if cache:
-            logger.debug("Search results from cache")
-            return json.loads(cache)
-        else:
-            logger.debug("Search results from service")
-            results = service.search(title, author, isbn)
-            kv_store.setex(self.CACHE_KEY_FORMAT.format(__name__, hash.hexdigest()), self.CACHE_EXPIRE, json.dumps(results))
-            return results
-
-
-def removeNonAscii(s):
-    if s:
-        return "".join(i for i in s if ord(i)<128)
-    else:
-        return ""
 
 
 class ResourceDetail(ServiceView):
