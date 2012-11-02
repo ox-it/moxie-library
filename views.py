3e168dbdf241df3da51318168bc28398cf98ac8e
Martin Filliau <martin@filliau.com>
2012-11-02 16:52:17 +0000

    Library / updated exceptions management.

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from flask import request, abort, url_for
 
 from moxie.core.views import ServiceView
 from moxie.library.services import LibrarySearchService
+from moxie.library.providers.oxford_z3950 import LibrarySearchException
+from moxie.library.providers.oxford_z3950 import LibrarySearchQuery
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +20,19 @@ class Search(ServiceView):
         start = int(request.args.get('start', 0))
         count = int(request.args.get('count', 10))
 
-        # TODO shouldn't be as general as Exception, could be Bad Request if it's inconsistent query
-        # or Service Unavailable if the service is... not available...
+        # TODO extract these exceptions from the provider
         try:
             service = LibrarySearchService.from_context()
             size, results = service.search(title, author, isbn, start, count)
-        except Exception as e:
-            abort(400, description=e.message)
+        except LibrarySearchException as e:
+            abort(500, description=e.msg)
+        except LibrarySearchQuery.InconsistentQuery as e:
+            abort(400, description=e.msg)
         else:
             # 2. Do pagination
             context = { 'size': size,
                         'results': results,
                         'links': dict()}
-            # TODO add links: prev, last in addition to next
             if size > start+count:
                 context['links']['next'] = url_for('.search', title=title, author=author, isbn=isbn, start=start+count, count=count)
             if start > 0 and size > start+count:
